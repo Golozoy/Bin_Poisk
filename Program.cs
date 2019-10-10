e33f3aadd5e4fb68c73ffbac06bfb0475e5683af
Golozoy <55829010+Golozoy@users.noreply.github.com>
2019-10-10 17:28:08 +1000

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Бинарный_поиск
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            
+            
+                int a = Sort.vibor();
+                int col = Sort.vvod(a);
+                int[] arr = new int[col];
+                Random rnd = new Random();
+                for (int i = 0; i < col; i++)
+                {
+                    arr[i] = rnd.Next(0, 100);
+                }
+                Console.WriteLine("Исходный массив:");
+                Sort.vivod(arr);
+                Sort.sort1(arr);
+                Console.WriteLine("\nОтсортированный массив:");
+                Sort.vivod(arr);
+            do { 
+                Console.Write("\n Ввести элемент поиска: ");
+                int target = Convert.ToInt32(Console.ReadLine());
+                int k = Sort.binPoisk(target, arr);
+                Console.WriteLine("Бинарный поиск:");
+                if (k > -1)
+                    Console.WriteLine("Индек запрошеного элемента = {0}", k);
+                else
+                    Console.WriteLine("Данного элимента нет в массиве");
+                //Console.WriteLine("Для выхода жмякни по клаве.");
+                //Console.ReadKey();
+                Console.WriteLine("Для выхода нажмите на q для повтора любой символ");
+
+                
+            }
+            while (Console.ReadLine() != "q");
+            
+            
+       }
+    }
+}
---
+++ Sort.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Бинарный_поиск
+{
+    class Sort
+    {
+        static public int[] sort1(int[] Arr)
+        {
+            List<int> CopiArr = new List<int>();
+            foreach (int el in Arr)
+                CopiArr.Add(el);
+            CopiArr.Sort();
+            CopiArr.CopyTo(Arr, 0);          
+            return Arr;
+        }
+
+        static public int binPoisk(int target, int[] array)
+        {
+            int mid;
+            int left = 0;
+            int right = array.Length - 1;
+            mid = (right + left) / 2;
+            while (left < right - 1)
+            {
+                mid = (right + left) / 2;
+                if (array[mid] == target)
+                    return mid;
+                if (array[mid] < target)
+                    left = mid;
+                else
+                    right = mid;
+            }
+            if (array[mid] != target)
+            {
+                if (array[left] == target)
+                    mid = left;
+                else
+                {
+                    if (array[right] == target)
+                        mid = right;
+                    else
+                        mid = -1;
+                };
+            }
+            return mid;
+        }
+        static public void vivod(int[] arr)
+        {
+            foreach (int el in arr)
+                Console.Write( $"{el} ");
+        }
+        static public int vvod(int a)
+        {
+            if (a != 7)
+            {
+                Random rnd = new Random();
+                
+                   int ass = rnd.Next(2, 20);
+                return ass; 
+            }
+            
+                Console.Write("Ведите положительное число элементов массива, используя цифры на клавеотере: \n ");
+                int k = Convert.ToInt32(Console.ReadLine());
+                return k;
+            
+            
+        }
+        static public int vibor()
+        {
+            Console.Write( "Если хотите задать размер массива-жмите на 7, в противном случии нажми на любую цифру и я решу за тебя ;) \n");
+            int a = Convert.ToInt32(Console.ReadLine());
+            return a;
+            
+        }
+    }
+}
+
